923920a58e7079eccd1175fcd82d46eed1c3b860
Markus Varckin XYZ <varckin@protonmail.com>
2025-10-23 17:26:19 +0300

    fix

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
+
 if __name__ == "__main__":
     try:
         logger.info('Start bot')
